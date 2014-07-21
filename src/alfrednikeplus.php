81e4b8c22f40d08b7ab32f56b1b2eca5cf249198
vdesabou <vincent.desaboulin@gmail.com>
2014-07-21 10:54:17 +0200

    Commented fuel band, I'm not there yet

--- src/alfrednikeplus.php
+++ src/alfrednikeplus.php
@@ -81,6 +81,87 @@ class AlfredNikePlusPHP extends NikePlusPHP {
     return $this->getMyLastFullActivity($week_offset + 1, FALSE);
   }
 
+/*
+  public function getAllFuelband($week_offset = 0, $monday_had_fuelpoints = FALSE) {
+	
+	$loop=false
+	$thismonth = strtotime('this month');
+
+    // Build the Year, Month and Monday strings for GMT for requested week.
+    $year = gmdate("Y", $monday_of_week_timestamp);	
+	$month = gmdate("n", $monday_of_week_timestamp);
+	
+	
+	while($loop == true) {
+	    // Build URL for request
+	    $url = 'https://secure-nikeplus.nike.com/plus/' . 'activity/fuelband/' . rawurlencode($this->userId) . '/week/' . $year . '/' . $month;
+	
+		echo "$url\n";
+	    $html = $this->_getHtml($url);
+	
+	    $activities = $this->searchActivity($html, 'baked_data');
+	    
+	    
+		if(isset($results->activities)) {
+			foreach($results->activities as $activity) {
+				$this->activities[$activity->activity->activityId] = $activity->activity;
+			}
+			$start += $increment + 1;
+			$limit += $start;
+		} else {
+			$loop = false;
+			break;
+		}
+	}
+				
+
+    // Build URL for request
+    $url = 'https://secure-nikeplus.nike.com/plus/' . 'activity/fuelband/' . rawurlencode($this->userId) . '/week/' . $year . '/' . $month;
+
+	echo "$url\n";
+    $html = $this->_getHtml($url);
+
+    $activities = $this->searchActivity($html, 'baked_data');
+    
+
+    // If $monday_had_fuelpoints == TRUE we were called recursively and the next day
+    // (Monday next week) has fuelpoints, so we can directly return the last day of
+    // this week (Sunday).
+    if ($monday_had_fuelpoints) {
+      return $activities->weeklyList[0]->dataPoints[6];
+    }
+
+    // $activities always contains a full week. We go backwards through it and search
+    // for an activity with fuelpoints.
+    for ($i=6; $i >= 0; $i--) {
+    	
+    	$tmp = $activities->weeklyList[0]->dataPoints[$i]->fuel;
+      echo "--- $tmp\n";
+      if ($activities->weeklyList[0]->dataPoints[$i]->fuel != 0 && $i == 0) {
+        // Found activity with fuelpoints but we are already on the Monday of the week.
+        // But because we cannot return one activity earlier, we load one week
+        // earlier and tell the function that Monday has fuelpoints.
+        
+        echo "Found activity with fuelpoints but we are already on the Monday of the week\n";
+        return $this->getMyLastFullActivity($week_offset + 1, TRUE);
+
+      }
+      elseif ($activities->weeklyList[0]->dataPoints[$i]->fuel != 0 && $i != 0) {
+        // Found activity with fuelpoints and we are not on Monday.
+        // Return one activity older than the found one.
+        
+        echo "Found activity with fuelpoints and we are not on Monday\n";
+        return $activities->weeklyList[0]->dataPoints[$i-1];
+      }
+    }
+    echo "No activity with fuelpoints this week, we load one week earlier.\n";
+    // No activity with fuelpoints this week, we load one week earlier.
+    return $this->getMyLastFullActivity($week_offset + 1, FALSE);
+  }
+*/
+  
+  
+  
   /**
    * Parses an html string to return the details of a specific activity.
    *
